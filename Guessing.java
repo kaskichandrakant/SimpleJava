757fdfd608f37b4a8c8d182437b77a778fb89d69
kaskichandrakant <chandk@in-chandk.local>
2018-04-06 23:39:59 +0530

    Gussing game will allow guessing only if player have enough turns left

--- Guessing.java
+++ Guessing.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Guessing {
     public static void main(String args[]){
-        int x,y;
+        int x,y,turns=10;
         Scanner input=new Scanner(System.in);
         System.out.println("enter the number tobe guessed");
         y=input.nextInt();
@@ -15,8 +15,14 @@ public class Guessing {
             if(x<y){
                 System.out.println("you entered the smaller number");
             }
+            turns--;
+            System.out.println(turns+" Turns Left");
+        }while(turns!=0&&x!=y);
+        if(x==y){
+          System.out.println("you Won");
+        }else{
+          System.out.println("you Lose");
 
-        }while(x!=y);
-        System.out.println("you are right");
+        }
     }
 }
---
+++ Pallindrome.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Pallindrome{
+  public static void main(String args[]){
+   Scanner input=new Scanner(System.in);
+   int num=input.nextInt();
+   int initial=num;
+ }
+}
